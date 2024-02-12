b4890b623b0d48b8807a0d7f5d391b8c78dea315
yusnarsetiyadi <yusnarsetiyadi150403@gmail.com>
2024-02-12 09:14:38 +0700

    fixing

--- be/app.js
+++ be/app.js
@@ -2,12 +2,16 @@ const express = require("express");
 const serveStatic = require("serve-static");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require('cors');
 const taskRouter = require('./route/task')
 const PORT = process.env.PORT;
 require("dotenv").config();
 
 app.use(serveStatic("fe"));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
 app.use('/task', taskRouter)
 
 app.listen(PORT, () => {
